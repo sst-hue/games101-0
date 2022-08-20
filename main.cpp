d66134ad3449ad44a76f3c9777c0833026696da2
issti <1261791531@qq.com>
2022-08-20 23:43:03 +0800

    增加：作业0程序

--- main.cpp
+++ main.cpp
@@ -2,23 +2,25 @@
 #include<eigen3/Eigen/Core>
 #include<eigen3/Eigen/Dense>
 #include<iostream>
+using namespace std;
+using namespace Eigen;
 
-int main(){
-
+void test()
+{
     // Basic Example of cpp
     std::cout << "Example of cpp \n";
     float a = 1.0, b = 2.0;
     std::cout << a << std::endl;
-    std::cout << a/b << std::endl;
+    std::cout << a / b << std::endl;
     std::cout << std::sqrt(b) << std::endl;
     std::cout << std::acos(-1) << std::endl;
-    std::cout << std::sin(30.0/180.0*acos(-1)) << std::endl;
+    std::cout << std::sin(30.0 / 180.0 * acos(-1)) << std::endl;
 
     // Example of vector
     std::cout << "Example of vector \n";
     // vector definition
-    Eigen::Vector3f v(1.0f,2.0f,3.0f);
-    Eigen::Vector3f w(1.0f,0.0f,0.0f);
+    Eigen::Vector3f v(1.0f, 2.0f, 3.0f);
+    Eigen::Vector3f w(1.0f, 0.0f, 0.0f);
     // vector output
     std::cout << "Example of output \n";
     std::cout << v << std::endl;
@@ -33,7 +35,7 @@ int main(){
     // Example of matrix
     std::cout << "Example of matrix \n";
     // matrix definition
-    Eigen::Matrix3f i,j;
+    Eigen::Matrix3f i, j;
     i << 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0;
     j << 2.0, 3.0, 1.0, 4.0, 6.0, 5.0, 9.0, 7.0, 8.0;
     // matrix output
@@ -43,6 +45,21 @@ int main(){
     // matrix scalar multiply i * 2.0
     // matrix multiply i * j
     // matrix multiply vector i * v
+}
 
+int main(){
+    Vector3f P(2.0f, 1.0f, 1.0f);
+    // -45¡ã
+    float x = -45.f / 180.f * acos(-1);
+    Matrix3f rotation45, move12;
+    // Ðý×ª¾ØÕó
+    rotation45 <<   cos(x), -sin(x), 0.f,
+                    sin(x), cos(x),  0.f,
+                    0.f,    0.f,     1.f;
+    move12 <<   1.f, 0.f, 1.f,
+                0.f, 1.f, 2.f,
+                0.f, 0.f, 1.f;
+    Vector3f target = rotation45 * P;
+    cout << "(" << target[0] << "," << target[1] << ")" << endl;
     return 0;
 }
